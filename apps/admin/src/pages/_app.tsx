72f22a61b9634f94f9a2409fef7b99a01b73f72a
Soulike <soulike@qq.com>
2023-05-03 16:31:37 +0800

    feat(admin): migrate login page

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,7 +1,38 @@
-import '@/styles/globals.css';
+import 'antd/dist/reset.css';
+import '@/pages/_app.scss';
 
+import {ConfigProvider} from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
+import {Suspense} from 'react';
+
+import {Loading} from '@/components/Loading';
 
 export default function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Suspense fallback={<Loading />}>
+                <ConfigProvider locale={zhCN}>
+                    <Component {...pageProps} />
+                </ConfigProvider>
+            </Suspense>
+            <Head>
+                <title>Admin</title>
+                <link rel='icon' href='/favicon.ico' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <meta name='theme-color' content='#000000' />
+                <meta
+                    content='noindex,nofollow,noarchive,nosnippet'
+                    name='robots'
+                />
+                <meta content='My admin app' name='description' />
+                <link href='https://unpkg.com' rel='preconnect' />
+                <link href='avatar.png' rel='apple-touch-icon' />
+            </Head>
+        </>
+    );
 }
