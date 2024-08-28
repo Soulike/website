904e556f4019292e97966045a1f1ea1a9780d66f
Soulike <git_anonymous@foxmail.com>
2024-08-28 09:02:32 +0800

    refactor(Blog): use new Request in options apis

--- apps/blog/src/apis/option/index.ts
+++ apps/blog/src/apis/option/index.ts
@@ -1,11 +1,8 @@
-import * as request from '@website/request';
-import {message} from 'antd';
+import {ServerResponse} from '@website/classes';
+import {Request} from '@website/request';
 
 import {GET_ABOUT} from './ROUTE';
 
-export async function get(): Promise<{about: string} | null> {
-  return await request.getServerResponse(GET_ABOUT, {
-    onRequestFail: (msg) => message.warning(msg),
-    onRequestError: (msg) => message.error(msg),
-  });
+export async function getAbout(): Promise<ServerResponse<{about: string}>> {
+  return await Request.JSONToJSON.get(GET_ABOUT);
 }
--- apps/blog/src/hooks/useAbout.ts
+++ apps/blog/src/hooks/useAbout.ts
@@ -1,6 +1,8 @@
+import {notification} from 'antd';
 import {useEffect, useState} from 'react';
 
 import {Option} from '@/src/apis';
+import {showNetworkError} from '@/src/apis/utils';
 
 export function useAbout(): {loading: boolean; about: string | null} {
   const [about, setAbout] = useState<string | null>(null);
@@ -8,11 +10,19 @@ export function useAbout(): {loading: boolean; about: string | null} {
 
   useEffect(() => {
     setLoading(true);
-    void Option.get()
-      .then((result) => {
-        if (result !== null) {
-          setAbout(result.about);
+    void Option.getAbout()
+      .then((response) => {
+        if (response.isSuccessful) {
+          const {
+            data: {about},
+          } = response;
+          setAbout(about);
+        } else {
+          notification.warning({message: response.message});
         }
+      })
+      .catch((e) => {
+        showNetworkError(e);
       })
       .finally(() => {
         setLoading(false);
