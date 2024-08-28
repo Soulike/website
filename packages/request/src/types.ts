843f8554adedc4e0285f67161cfc4ee82b4bd65f
Soulike <git_anonymous@foxmail.com>
2024-08-28 09:06:15 +0800

    refactor(request): remove deprecated code

--- packages/request/src/types.ts
+++ packages/request/src/types.ts
@@ -2,24 +2,3 @@ export interface RequestOptions {
   headers?: Headers;
   searchParams?: URLSearchParams;
 }
-
-export type RequestSuccessChecker<ResT, DataT> = (
-  response: ResT,
-  data: DataT,
-) => boolean | Promise<boolean>;
-
-export type ResponseTransformer<ResT, DataT> = (
-  response: ResT,
-) => DataT | Promise<DataT>;
-
-type RequestResultHandler<ResT, DataT> = (
-  response: ResT,
-  data: DataT,
-) => unknown | Promise<unknown>;
-export type RequestSuccessHandler<ResT, DataT> = RequestResultHandler<
-  ResT,
-  DataT
->;
-export type RequestFailHandler<ResT, DataT> = RequestResultHandler<ResT, DataT>;
-
-export type RequestErrorHandler = (e: unknown) => unknown | Promise<unknown>;
