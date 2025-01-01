4af527a7724d69a1f0a0b8aeffd5ac3a6f8c32e1
Soulike <git_anonymous@foxmail.com>
2025-01-01 14:58:22 +0800

    chore: move eslint back to global

---
+++ lint-staged.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * By default, we use global executables for all apps and packages.
+ * If any app/package needs to use their own executables,
+ * create lint-staged.config.mjs under its own folder,
+ * and import configs from this file if needed.
+ *
+ * See https://github.com/lint-staged/lint-staged?tab=readme-ov-file#how-to-use-lint-staged-in-a-multi-package-monorepo.
+ * */
+export const prettierConfig = Object.freeze({
+  '*': ['prettier --write --ignore-unknown'],
+});
+
+export const eslintConfig = Object.freeze({
+  './**/src/**/*.?(m)(j|t)(s|sx)': ['eslint --fix'],
+});
+
+export default {
+  ...prettierConfig,
+  ...eslintConfig,
+};
