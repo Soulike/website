1ff5c949c1f247ab394db5f6fd53dc78b0d1f448
Soulike <git_anonymous@foxmail.com>
2025-01-05 12:49:12 +0800

    chore: add comments for eslint flag

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -12,6 +12,9 @@ export const prettierConfig = Object.freeze({
 
 export const eslintConfig = Object.freeze({
   './**/src/**/*.?(m)(j|t)(s|sx)': [
+    // We use a flag here to ensure eslint uses correct config files.
+    // The flag should be stable in eslint v10,
+    // see https://eslint.org/docs/v9.x/use/configure/configuration-files#experimental-configuration-file-resolution
     'eslint --flag unstable_config_lookup_from_file --fix',
   ],
 });
