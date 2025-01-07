f7290109e44236b2c878618d05c9b21b2dc622a7
Soulike <git_anonymous@foxmail.com>
2025-01-07 21:12:58 +0800

    chore: update react eslint config

--- apps/admin-vite/eslint.config.js
+++ apps/admin-vite/eslint.config.js
@@ -1,17 +1,14 @@
 import reactRefresh from 'eslint-plugin-react-refresh';
-import reactHooks from 'eslint-plugin-react-hooks';
 import eslintConfig from '@website/eslint-config';
 
 export default [
-  ...eslintConfig.recommendedTypeScript,
+  ...eslintConfig.recommendedReactTs,
   {ignores: ['dist']},
   {
     plugins: {
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         {allowConstantExport: true},
--- packages/hooks/eslint.config.mjs
+++ packages/hooks/eslint.config.mjs
@@ -1,14 +1,3 @@
 import eslintConfig from '@website/eslint-config';
-import hooksPlugin from 'eslint-plugin-react-hooks';
 
-export default [
-  ...eslintConfig.recommendedTypeScript,
-  {
-    plugins: {
-      'react-hooks': hooksPlugin,
-    },
-    rules: {
-      ...hooksPlugin.configs.recommended.rules,
-    },
-  },
-];
+export default [...eslintConfig.recommendedReactTs];
