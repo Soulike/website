da171f7a327be84fbb23a1cd4b34958011cb1e05
Soulike <git_anonymous@foxmail.com>
2025-02-23 15:35:58 +0800

    feat: continue setup blog-vite template

--- apps/blog-vite/src/main.tsx
+++ apps/blog-vite/src/main.tsx
@@ -1,9 +1,18 @@
+import './main.css';
+
+import assert from 'node:assert';
+
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './index.css';
+
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const rootHTMLElement = document.getElementById('root');
+assert(rootHTMLElement, 'The root HTMLElement for React is missing.');
+
+const reactRoot = createRoot(rootHTMLElement);
+
+reactRoot.render(
   <StrictMode>
     <App />
   </StrictMode>,
