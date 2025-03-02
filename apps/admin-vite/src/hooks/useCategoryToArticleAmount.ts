1f378cd2d073d6c7944575ba9a14e35778fc77e8
Soulike <git_anonymous@foxmail.com>
2025-03-02 20:01:13 +0800

    feat: update useCategories and add useArticleAmountGroupedById

--- apps/admin-vite/src/hooks/useCategoryToArticleAmount.ts
+++ apps/admin-vite/src/hooks/useCategoryToArticleAmount.ts
@@ -2,15 +2,15 @@ import {usePromise} from '@website/hooks';
 import {BlogModels} from '@website/model';
 import {useMemo} from 'react';
 
-export function useCategories() {
+export function useArticleAmountGroupedById() {
   const categoryModel = useMemo(() => new BlogModels.CategoryModel(), []);
   const {pending, resolvedValue, rejectedError} = usePromise(
-    categoryModel.getAll(),
+    categoryModel.getArticleAmountGroupedById(),
   );
 
   return {
     loading: pending,
     error: rejectedError,
-    categories: resolvedValue,
+    articleAmountGroupedById: resolvedValue,
   };
 }
