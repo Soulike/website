86945889b3439bc31514ef6adc9d693087ad17f4
Soulike <git_anonymous@foxmail.com>
2025-03-05 08:13:40 +0800

    feat: implement article modification view model and update container logic

--- apps/admin-vite/src/pages/manage/blog/article/modify/container.tsx
+++ apps/admin-vite/src/pages/manage/blog/article/modify/container.tsx
@@ -1,162 +1,98 @@
-import {Blog} from '@website/server-api';
-import {
-  type ButtonProps,
-  type CheckboxProps,
-  type InputProps,
-  message,
-  type ModalProps,
-  notification,
-  type SelectProps,
-} from 'antd';
-import {TextAreaProps} from 'antd/lib/input';
-import {useEffect, useState} from 'react';
-import {useNavigate, useSearchParams} from 'react-router';
+import assert from 'node:assert';
+
+import {ModelAccessDeniedError} from '@website/model';
+import {type ButtonProps, notification} from 'antd';
+import {useEffect} from 'react';
 
 import {showNetworkError} from '@/helpers/error-notification-helper.js';
-import {useCategories} from '@/hooks/useCategories';
-import {PAGE_ID, PAGE_ID_TO_PATH} from '@/router/page-config';
 
 import {ModifyView} from './view.js';
+import {useViewModel} from './view-model.js';
 
 export function Modify() {
-  const [id, setId] = useState(0);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [category, setCategory] = useState<number | undefined>(undefined);
-  const [isVisible, setIsVisible] = useState(true);
-  const {loading: isLoadingCategories, categories} = useCategories();
-  const [isLoadingArticle, setIsLoadingArticle] = useState(false);
-  const [isSubmittingArticle, setIsSubmittingArticle] = useState(false);
-  const [isArticlePreviewModalOpen, setIsArticlePreviewModalOpen] =
-    useState(false);
-
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
+  const {
+    articleLoading,
+    articleLoadError,
+    categories,
+    categoriesLoading,
+    categoriesLoadError,
+    title,
+    onTitleInputChange,
+    content,
+    onContentInputChange,
+    selectedCategory,
+    onCategorySelectChange,
+    isVisibleChecked,
+    onIsVisibleCheckboxChange,
+    showArticlePreviewModal,
+    hideArticlePreviewModal,
+    articlePreviewModalVisible,
+    modifyingArticleId,
+    isSubmittingArticleModification,
+    handleArticleModificationSubmit,
+  } = useViewModel();
 
   useEffect(() => {
-    // 查询字符串格式为 ?id=xxx
-    const idString = searchParams.get('id');
-    if (idString === null) {
-      void message.warning('Invalid parameters');
-    } else {
-      const id = Number.parseInt(idString);
-      if (Number.isNaN(id)) {
-        void message.warning('Invalid parameters');
+    if (categoriesLoadError) {
+      if (categoriesLoadError instanceof ModelAccessDeniedError) {
+        notification.error({message: categoriesLoadError.message});
       } else {
-        // 获取文章内容
-        setId(id);
-        setIsLoadingArticle(true);
-        void Blog.Article.getById(id)
-          .then((response) => {
-            if (response.isSuccessful) {
-              const {data: article} = response;
-              const {title, content, category, isVisible} = article;
-              setTitle(title);
-              setContent(content);
-              setCategory(category);
-              setIsVisible(isVisible);
-            } else {
-              const {message} = response;
-              notification.warning({message});
-            }
-          })
-          .catch((err: unknown) => {
-            showNetworkError(err);
-          })
-          .finally(() => {
-            setIsLoadingArticle(false);
-          });
+        showNetworkError(categoriesLoadError);
       }
     }
-  }, [searchParams]);
 
-  const onTitleInputChange: InputProps['onChange'] = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const onContentTextAreaChange: TextAreaProps['onChange'] = (e) => {
-    setContent(e.target.value);
-  };
-
-  const onCategorySelectorChange: SelectProps<number>['onChange'] = (value) => {
-    setCategory(value); // 在 View 中设置的是 number
-  };
-
-  const onIsVisibleCheckboxChange: CheckboxProps['onChange'] = (e) => {
-    setIsVisible(e.target.checked);
-  };
-
-  const onArticlePreviewButtonClick: ButtonProps['onClick'] = (e) => {
-    e.preventDefault();
-    setIsArticlePreviewModalOpen(true);
-  };
-
-  const onArticlePreviewModalOk: ModalProps['onOk'] = (e) => {
-    e.preventDefault();
-    setIsArticlePreviewModalOpen(false);
-  };
-
-  const onArticlePreviewModalCancel: ModalProps['onCancel'] =
-    onArticlePreviewModalOk;
+    if (articleLoadError) {
+      if (articleLoadError instanceof ModelAccessDeniedError) {
+        notification.error({message: articleLoadError.message});
+      } else {
+        showNetworkError(articleLoadError);
+      }
+    }
+  }, [articleLoadError, categoriesLoadError]);
 
   const onSubmitButtonClick: ButtonProps['onClick'] = (e) => {
     e.preventDefault();
-    const executor = async () => {
-      if (typeof category === 'undefined') {
-        await message.warning('请选择文章分类');
-      } else if (title.length === 0) {
-        await message.warning('请填写文章标题');
-      } else if (content.length === 0) {
-        await message.warning('请填写文章内容');
-      } else {
-        try {
-          setIsSubmittingArticle(true);
-          const response = await Blog.Article.modify({
-            id,
-            title,
-            content,
-            category,
-            isVisible,
-          });
-          if (response.isSuccessful) {
-            notification.success({message: 'Article modified'});
-            await navigate(
-              PAGE_ID_TO_PATH[PAGE_ID.MANAGE.BLOG.ARTICLE.MANAGE],
-              {replace: true},
-            );
-          } else {
-            const {message} = response;
-            notification.warning({message});
-          }
-        } catch (err) {
-          showNetworkError(err);
-        } finally {
-          setIsSubmittingArticle(false);
+    assert(modifyingArticleId !== null);
+    handleArticleModificationSubmit(
+      modifyingArticleId,
+      title,
+      content,
+      selectedCategory,
+      isVisibleChecked,
+      (message: string) => {
+        notification.error({message});
+      },
+      () => {
+        notification.success({message: 'Article modified'});
+      },
+      (error) => {
+        if (error instanceof ModelAccessDeniedError) {
+          notification.error({message: error.message});
+        } else {
+          showNetworkError(error);
         }
-      }
-    };
-
-    void executor();
+      },
+    );
   };
 
   return (
     <ModifyView
       title={title}
       content={content}
-      category={category}
+      selectedCategory={selectedCategory}
       categoryOption={categories ?? []}
-      isLoadingCategory={isLoadingCategories}
-      isLoadingArticle={isLoadingArticle}
-      isSubmittingArticle={isSubmittingArticle}
-      isVisible={isVisible}
-      onArticlePreviewButtonClick={onArticlePreviewButtonClick}
-      onArticlePreviewModalOk={onArticlePreviewModalOk}
-      onArticlePreviewModalCancel={onArticlePreviewModalCancel}
-      isArticlePreviewModalOpen={isArticlePreviewModalOpen}
+      isLoadingCategory={categoriesLoading}
+      isLoadingArticle={articleLoading}
+      isSubmittingArticle={isSubmittingArticleModification}
+      isVisible={isVisibleChecked}
+      onArticlePreviewButtonClick={showArticlePreviewModal}
+      onArticlePreviewModalOk={hideArticlePreviewModal}
+      onArticlePreviewModalCancel={hideArticlePreviewModal}
+      isArticlePreviewModalOpen={articlePreviewModalVisible}
       onSubmitButtonClick={onSubmitButtonClick}
       onIsVisibleCheckboxChange={onIsVisibleCheckboxChange}
-      onCategorySelectorChange={onCategorySelectorChange}
-      onContentTextAreaChange={onContentTextAreaChange}
+      onCategorySelectorChange={onCategorySelectChange}
+      onContentTextAreaChange={onContentInputChange}
       onTitleInputChange={onTitleInputChange}
     />
   );
