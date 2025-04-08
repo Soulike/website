6a4c7bdfedba15ae93696de2634c983f7b2a09ef
Soulike <git_anonymous@foxmail.com>
2025-04-08 21:38:43 +0800

    fix: fix useColorScheme export

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -2,6 +2,7 @@ export {useAntdCheckbox} from './useAntdCheckbox.js';
 export {useAntdSelect} from './useAntdSelect.js';
 export {useAntdSwitch} from './useAntdSwitch.js';
 export * from './useColorScheme.js';
+export {ColorScheme, useColorScheme} from './useColorScheme.js';
 export * from './useMediaQuery.js';
 export {useModal} from './useModal.js';
 export {RejectCallback, ResolveCallback, usePromise} from './usePromise.js';
