d9f1d7cdcd2c1b4b6c7876c9645dc0b175bd945d
Soulike <git_anonymous@foxmail.com>
2025-04-15 07:31:47 +0800

    feat: add labels in i18n

--- packages/i18n/src/strings/en.ts
+++ packages/i18n/src/strings/en.ts
@@ -4,4 +4,7 @@ import {Strings} from './Strings.js';
 export const EN: Strings = Object.freeze({
   [STRING_KEY.TEST_STRING]: 'Test string',
   [STRING_KEY.UI_MESSAGE_NETWORK_ERROR]: 'Network error',
+  [STRING_KEY.UI_LABEL_INDEX]: 'Index',
+  [STRING_KEY.UI_LABEL_CATEGORY]: 'Category',
+  [STRING_KEY.UI_LABEL_ABOUT]: 'About',
 });
--- packages/i18n/src/strings/zh-cn.ts
+++ packages/i18n/src/strings/zh-cn.ts
@@ -4,4 +4,7 @@ import {Strings} from './Strings.js';
 export const ZH_CN: Strings = Object.freeze({
   [STRING_KEY.TEST_STRING]: '测试字符串',
   [STRING_KEY.UI_MESSAGE_NETWORK_ERROR]: '网络错误',
+  [STRING_KEY.UI_LABEL_INDEX]: '首页',
+  [STRING_KEY.UI_LABEL_CATEGORY]: '分类',
+  [STRING_KEY.UI_LABEL_ABOUT]: '关于',
 });
