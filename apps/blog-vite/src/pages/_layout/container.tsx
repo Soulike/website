0a883881cc2f4da9ee97333f07708cb7dadf035c
Soulike <git_anonymous@foxmail.com>
2025-04-20 15:19:32 +0800

    fix: fix global wrappers are in layout instead of main

--- apps/blog-vite/src/pages/_layout/container.tsx
+++ apps/blog-vite/src/pages/_layout/container.tsx
@@ -1,23 +1,11 @@
-import {ErrorBoundary} from '@website/react-components';
-import {Suspense} from 'react';
 import {Outlet} from 'react-router';
-
-import {Loading} from '@/components/Loading/index.js';
-import {NotFound} from '@/components/NotFound/index.js';
-import {ThemeProvider} from '@/components/ThemeProvider';
 
 import {RootLayoutView} from './view.js';
 
 export function RootLayout() {
   return (
-    <ErrorBoundary fallback={<NotFound />}>
-      <Suspense fallback={<Loading />}>
-        <ThemeProvider>
-          <RootLayoutView>
-            <Outlet />
-          </RootLayoutView>
-        </ThemeProvider>
-      </Suspense>
-    </ErrorBoundary>
+    <RootLayoutView>
+      <Outlet />
+    </RootLayoutView>
   );
 }
