ddb52ced9ad33480d6a824d08fa0211ad89d02c3
Soulike <git_anonymous@foxmail.com>
2025-07-12 15:33:49 +0800

    feat: continue finishing 2048 game boilerplate

--- apps/web/game-2048/vite.config.ts
+++ apps/web/game-2048/vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 import {defineConfig} from 'vite';
+import {ViteImageOptimizer} from 'vite-plugin-image-optimizer';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [nodePolyfills(), tsconfigPaths(), react(), ViteImageOptimizer()],
+  build: {
+    target: browserslistToEsbuild(),
+  },
 });
