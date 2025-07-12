d7511a9a08c5c8e3c9517d4548a996d71b399d43
Soulike <git_anonymous@foxmail.com>
2025-07-12 16:46:08 +0800

    fix: fix eslint config

--- apps/web/game-2048/eslint.config.js
+++ apps/web/game-2048/eslint.config.js
@@ -1,24 +1,18 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import {globalIgnores} from 'eslint/config';
 import eslintConfig from '@website/eslint-config';
 
-export default tseslint.config([
+export default [
   ...eslintConfig.recommendedReactTs,
-  globalIgnores(['dist']),
+  {ignores: ['dist']},
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {allowConstantExport: true},
+      ],
     },
   },
-]);
+];
