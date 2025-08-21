af019383a4e30074fb6b38e414f611519d274b4a
Soulike <git_anonymous@foxmail.com>
2025-08-21 08:24:47 +0800

    chore: upgrade dependencies

--- apps/server/auth/src/configurations/koa-body-options.ts
+++ apps/server/auth/src/configurations/koa-body-options.ts
@@ -1,8 +1,8 @@
-import {type koaBody} from 'koa-body';
+import {bodyParser} from '@koa/bodyparser';
+import {Context} from 'koa';
 
-export const KOA_BODY_OPTIONS: Parameters<typeof koaBody>[0] = {
-  multipart: true,
-  onError: (err, ctx) => {
-    ctx.throw(400, 'Unparsable request body');
+export const KOA_BODY_OPTIONS: Parameters<typeof bodyParser>[0] = {
+  onError: (err, ctx: Context) => {
+    ctx.throw(400, `Unparsable request body\n${err}`);
   },
 };
